5144fa85cc10fad636362db68444d6ece3d1bc6f
Juan Ibanez <juan.ibanezdf@gmail.com>
2024-12-29 13:16:02 -0300

    feat: add server api

---
+++ api/server.go
@@ -0,0 +1,32 @@
+package api
+
+import (
+	db "github.com/Juan-Ibanezdf/gofinance-backend/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+type Server struct {
+	store  *db.SQLStore
+	router *gin.Engine
+}
+
+func NewServer(store *db.SQLStore) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	// vamos por nossas rotas
+	router.POST("/user", server.createUser)
+	router.GET("/user/:username", server.getUser)
+	router.POST("/user/:id", server.getUserById)
+
+	server.router = router
+	return server
+}
+
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"Api has error: ": err.Error()}
+}
